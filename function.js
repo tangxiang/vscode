ceac71f0fde0dd2697f3b899620270d6b39157ba
Alex Tang <atang@ci.com>
2019-01-30 16:06:09 -0500

    function and object

---
+++ function.js
@@ -0,0 +1,18 @@
+function greet(firstName, lastName) {
+    console.log('Hello ' + firstName + ' ' + lastName);
+}
+
+greet('Alex', 'Tang');
+
+
+function square(number) {
+    console.log(number*number);
+}
+
+square(4);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+add(100.0, 22);
---
+++ test.js
@@ -0,0 +1,18 @@
+const val = 1;
+let name1 ='alex';
+let age = 31;
+let colors = ['black', 'red', 'blue'];
+let person = {
+    name: 'iris',
+    age: 32,
+    colors: ['green', 'white']
+}
+
+person.name = name1;
+person.colors = colors;
+
+console.log(colors);
+console.log(person);
+
+console.log(person.age);
+console.log(person.name);
